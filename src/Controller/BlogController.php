b752362125300288cedf15697a9da08d3cf99174
Greg Holmes <greg.holmes@housesimple.com>
2018-02-12 15:10:19 +0000

    Added first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+class BlogController extends Controller
+{
+    /** @var integer */
+    const POST_LIMIT = 5;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $blogPostRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     * @Route("/entries", name="entries")
+     */
+    public function entriesAction(Request $request)
+    {
+        $page = 1;
+
+        if ($request->get('page')) {
+            $page = $request->get('page');
+        }
+
+        return $this->render('Blog/entries.html.twig', [
+            'blogPosts' => $this->blogPostRepository->getAllPosts($page, self::POST_LIMIT),
+            'totalBlogPosts' => $this->blogPostRepository->getPostCount(),
+            'page' => $page,
+            'entryLimit' => self::POST_LIMIT
+        ]);
+    }
+}
